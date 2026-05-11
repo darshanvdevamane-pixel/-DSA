4025da24e3d04721380769a33bfd029ab59eb0d3
DARSHAN DEVAMANE <darshan.vdevamane@cognizant.com>
2026-05-11 09:50:52 +0530

    Queue

---
+++ Queue/Array/ConsoleApp1/Program.cs
@@ -0,0 +1,129 @@
+﻿//namespace ConsoleApp1;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Hello, World!");
+//    }
+//}
+
+
+namespace ConsoleApp1;
+
+class QueueArray
+{
+    int[] data;
+    int front;
+    int tail;
+    int size = 0;
+
+    public QueueArray(int n)
+    {
+        data = new int[n];
+        front = 0;
+        tail = 0;
+
+    }
+
+    public int length()
+    {
+        return size; ;
+    }
+
+    public bool isEmpty()
+    {
+        return size == 0;
+    }
+
+    public bool isFull()
+    {
+        return size == data.Length;
+    }
+
+    public void enqueue(int e)
+    {
+        if (isFull())
+        {
+            Console.WriteLine("Stack is full");
+            return;
+        }
+        else
+        {
+            data[tail] = e;
+            tail++;
+            size++;
+        }
+
+    }
+
+    public int dequeue()
+    {
+        if (isEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+            return -1;
+        }
+        else
+        {
+            int e = data[front];
+            front++;
+            size--;
+            return e;
+        }
+    }
+
+    public int first()
+    {
+        if (isEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+            return -1;
+        }
+        else
+        {
+            return data[front];
+        }
+    }
+
+
+    public void display()
+    {
+        int p = front;
+        while (p <= tail-1)
+        {
+            Console.WriteLine(data[p]);
+            p++;
+        }
+        Console.WriteLine();
+    }
+    static void Main(string[] args)
+    {
+        QueueArray l = new QueueArray(10);
+        Console.WriteLine("Enqueuing the elements");
+        l.enqueue(13);
+        l.enqueue(23);
+        l.enqueue(323);
+        l.enqueue(403);
+        l.enqueue(32);
+        l.enqueue(24);
+        l.enqueue(465);
+        Console.WriteLine();
+        Console.WriteLine("Checking the first element in the queue");
+        Console.WriteLine("First element in queue: " + l.first());
+        Console.WriteLine();
+        Console.WriteLine("Dsiplaying the element");
+        l.display();
+        Console.WriteLine();
+        Console.WriteLine("Dequeuing the elements");
+        Console.WriteLine("Dequeued Element: " + l.dequeue());
+        Console.WriteLine("Dequeued Element: " + l.dequeue());
+        Console.WriteLine("Dequeued Element: " + l.dequeue());
+        Console.WriteLine("Dequeued Element: " + l.dequeue());
+        Console.WriteLine();
+        Console.WriteLine("Dsiplaying the element");
+        l.display();
+        Console.WriteLine("Checking the first element in the queue");
+        Console.WriteLine("First element in queue: " + l.first());
+    }
+}
---
+++ Queue/LinkedList/ConsoleApp1/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace ConsoleApp1;
+
+class Node
+{
+    public int element;
+    public Node next;
+    public Node(int e,Node n)
+    {
+        element = e;
+        next = n;
+    }
+}
+class LinkedQueue
+{
+    private Node head;
+    private Node tail;
+    private int size = 0;
+    public LinkedQueue()
+    {
+        head = null;
+        tail = null;
+    }
+
+    public bool isEmpty()
+    {
+        return size == 0;
+    }
+
+    public void enqueue(int e)
+    {
+        Node newest = new Node(e, null);
+        if (isEmpty())
+        {
+            head = newest;
+        }
+        else
+        {
+            tail.next = newest;
+        }
+        tail = newest;
+        size++;
+    }
+
+    public int dequeue()
+    {
+        if (isEmpty())
+        {
+            Console.WriteLine("Queue is Empty");
+            return -1;
+        }
+        int n = head.element;
+        head = head.next;
+        size--;
+        if (isEmpty())
+        {
+            tail = null;
+        }
+        return n;
+    }
+    public int first()
+    {
+        if (isEmpty())
+        {
+            Console.WriteLine("Queue is empty");
+            return -1;
+        }
+        return head.element;
+    }
+
+    public void display()
+    {
+        Node p = head;
+        int i = 0;
+        while(i <= size-1)
+        {
+            Console.WriteLine(p.element);
+            p = p.next;
+            i++;
+        }
+    }
+    static void Main(string[] args)
+    {
+        LinkedQueue l = new LinkedQueue();
+        Console.WriteLine("Enqueuing the elements");
+        l.enqueue(13);
+        l.enqueue(23);
+        l.enqueue(323);
+        l.enqueue(403);
+        l.enqueue(32);
+        l.enqueue(24);
+        l.enqueue(465);
+        Console.WriteLine();
+        Console.WriteLine("Checking the first element in the queue");
+        Console.WriteLine("First element in queue: "+ l.first());
+        Console.WriteLine();
+        Console.WriteLine("Dsiplaying the element");
+        l.display();
+        Console.WriteLine();
+        Console.WriteLine("Dequeuing the elements");
+        Console.WriteLine("Dequeued Element: "+ l.dequeue());
+        Console.WriteLine("Dequeued Element: " + l.dequeue());
+        Console.WriteLine("Dequeued Element: " + l.dequeue());
+        Console.WriteLine("Dequeued Element: " + l.dequeue());
+        Console.WriteLine();
+        Console.WriteLine("Dsiplaying the element");
+        l.display();
+        Console.WriteLine("Checking the first element in the queue");
+        Console.WriteLine("First element in queue: " + l.first());
+
+    }
+}
